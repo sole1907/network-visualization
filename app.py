6375c3a209bcf328800d0f56c54e8cbb1f0afed6
ayansola_akanmu <ayansola_akanmu@mastercard.com>
2020-12-20 12:40:54 +0300

    color/style changes

--- app.py
+++ app.py
@@ -17,53 +17,60 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Transaction Network"
 
-YEAR=[2010, 2019]
-ACCOUNT="A0001"
+YEAR = [2010, 2019]
+ACCOUNT = "A0001"
 
 ##############################################################################################################################################################
+
+
 def network_graph(yearRange, AccountToSearch):
 
     edge1 = pd.read_csv('edge1.csv')
     node1 = pd.read_csv('node1.csv')
 
     # filter the record by datetime, to enable interactive control through the input box
-    edge1['Datetime'] = "" # add empty Datetime column to edge1 dataframe
-    accountSet=set() # contain unique account
-    for index in range(0,len(edge1)):
-        edge1['Datetime'][index] = datetime.strptime(edge1['Date'][index], '%d/%m/%Y')
-        if edge1['Datetime'][index].year<yearRange[0] or edge1['Datetime'][index].year>yearRange[1]:
+    edge1['Datetime'] = ""  # add empty Datetime column to edge1 dataframe
+    accountSet = set()  # contain unique account
+    for index in range(0, len(edge1)):
+        edge1['Datetime'][index] = datetime.strptime(
+            edge1['Date'][index], '%d/%m/%Y')
+        if edge1['Datetime'][index].year < yearRange[0] or edge1['Datetime'][index].year > yearRange[1]:
             edge1.drop(axis=0, index=index, inplace=True)
             continue
         accountSet.add(edge1['Source'][index])
         accountSet.add(edge1['Target'][index])
 
     # to define the centric point of the networkx layout
-    shells=[]
-    shell1=[]
+    shells = []
+    shell1 = []
     shell1.append(AccountToSearch)
     shells.append(shell1)
-    shell2=[]
+    shell2 = []
     for ele in accountSet:
-        if ele!=AccountToSearch:
+        if ele != AccountToSearch:
             shell2.append(ele)
     shells.append(shell2)
+    print("=========shells=============")
+    print(shells)
 
-
-    G = nx.from_pandas_edgelist(edge1, 'Source', 'Target', ['Source', 'Target', 'TransactionAmt', 'Date'], create_using=nx.MultiDiGraph())
-    nx.set_node_attributes(G, node1.set_index('Account')['CustomerName'].to_dict(), 'CustomerName')
-    nx.set_node_attributes(G, node1.set_index('Account')['Type'].to_dict(), 'Type')
+    G = nx.from_pandas_edgelist(df=edge1, source='Source', target='Target', edge_attr=[
+                                'Source', 'Target', 'TransactionAmt', 'Date'], create_using=nx.MultiDiGraph())
+    nx.set_node_attributes(G, node1.set_index('Account')[
+                           'CustomerName'].to_dict(), 'CustomerName')
+    nx.set_node_attributes(G, node1.set_index(
+        'Account')['Type'].to_dict(), 'Type')
     # pos = nx.layout.spring_layout(G)
     # pos = nx.layout.circular_layout(G)
     # nx.layout.shell_layout only works for more than 3 nodes
-    if len(shell2)>1:
+    if len(shell2) > 1:
         pos = nx.drawing.layout.shell_layout(G, shells)
     else:
         pos = nx.drawing.layout.spring_layout(G)
+
     for node in G.nodes:
         G.nodes[node]['pos'] = list(pos[node])
 
-
-    if len(shell2)==0:
+    if len(shell2) == 0:
         traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
 
         node_trace = go.Scatter(x=tuple([1]), y=tuple([1]), text=tuple([str(AccountToSearch)]), textposition="bottom center",
@@ -72,32 +79,36 @@ def network_graph(yearRange, AccountToSearch):
         traceRecode.append(node_trace)
 
         node_trace1 = go.Scatter(x=tuple([1]), y=tuple([1]),
-                                mode='markers',
-                                marker={'size': 50, 'color': 'LightSkyBlue'},
-                                opacity=0)
+                                 mode='markers',
+                                 marker={'size': 50, 'color': 'LightSkyBlue'},
+                                 opacity=0)
         traceRecode.append(node_trace1)
 
         figure = {
             "data": traceRecode,
             "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False,
                                 margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
-                                xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                                yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                                xaxis={'showgrid': False, 'zeroline': False,
+                                       'showticklabels': False},
+                                yaxis={'showgrid': False, 'zeroline': False,
+                                       'showticklabels': False},
                                 height=600
                                 )}
         return figure
 
-
     traceRecode = []  # contains edge_trace, node_trace, middle_node_trace
     ############################################################################################################################################################
-    colors = list(Color('lightcoral').range_to(Color('darkred'), len(G.edges())))
+    colors = list(Color('lightblue').range_to(
+        Color('darkblue'), len(G.edges())))
     colors = ['rgb' + str(x.rgb) for x in colors]
 
     index = 0
     for edge in G.edges:
         x0, y0 = G.nodes[edge[0]]['pos']
         x1, y1 = G.nodes[edge[1]]['pos']
-        weight = float(G.edges[edge]['TransactionAmt']) / max(edge1['TransactionAmt']) * 10
+        weight = float(G.edges[edge]['TransactionAmt']) / \
+            max(edge1['TransactionAmt']) * 5
+        
         trace = go.Scatter(x=tuple([x0, x1, None]), y=tuple([y0, y1, None]),
                            mode='lines',
                            line={'width': weight},
@@ -108,7 +119,9 @@ def network_graph(yearRange, AccountToSearch):
         index = index + 1
     ###############################################################################################################################################################
     node_trace = go.Scatter(x=[], y=[], hovertext=[], text=[], mode='markers+text', textposition="bottom center",
-                            hoverinfo="text", marker={'size': 50, 'color': 'LightSkyBlue'})
+                            hoverinfo="text", marker=dict(color=[],
+                                                          size=[],
+                                                          line=None))
 
     index = 0
     for node in G.nodes():
@@ -116,16 +129,28 @@ def network_graph(yearRange, AccountToSearch):
         hovertext = "CustomerName: " + str(G.nodes[node]['CustomerName']) + "<br>" + "AccountType: " + str(
             G.nodes[node]['Type'])
         text = node1['Account'][index]
+        acc_type = node1['Type'][index]
+        color = 'LightSkyBlue'
+        size = 50
+        if acc_type == 'Type2':
+            color = 'DodgerBlue'
+            size = 75
+        elif acc_type == 'Type3':
+            color = 'PowderBlue'
+            size = 100
         node_trace['x'] += tuple([x])
         node_trace['y'] += tuple([y])
         node_trace['hovertext'] += tuple([hovertext])
         node_trace['text'] += tuple([text])
+        node_trace['marker']['color'] += tuple([color])
+        node_trace['marker']['size'] += tuple([size])
         index = index + 1
 
     traceRecode.append(node_trace)
     ################################################################################################################################################################
     middle_hover_trace = go.Scatter(x=[], y=[], hovertext=[], mode='markers', hoverinfo="text",
-                                    marker={'size': 20, 'color': 'LightSkyBlue'},
+                                    marker={'size': 20,
+                                            'color': 'LightSkyBlue'},
                                     opacity=0)
 
     index = 0
@@ -146,24 +171,30 @@ def network_graph(yearRange, AccountToSearch):
         "data": traceRecode,
         "layout": go.Layout(title='Interactive Transaction Visualization', showlegend=False, hovermode='closest',
                             margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
-                            xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
-                            yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                            xaxis={'showgrid': False, 'zeroline': False,
+                                   'showticklabels': False},
+                            yaxis={'showgrid': False, 'zeroline': False,
+                                   'showticklabels': False},
                             height=600,
                             clickmode='event+select',
                             annotations=[
                                 dict(
-                                    ax=(G.nodes[edge[0]]['pos'][0] + G.nodes[edge[1]]['pos'][0]) / 2,
+                                    ax=(G.nodes[edge[0]]['pos'][0] +
+                                        G.nodes[edge[1]]['pos'][0]) / 2,
                                     ay=(G.nodes[edge[0]]['pos'][1] + G.nodes[edge[1]]['pos'][1]) / 2, axref='x', ayref='y',
-                                    x=(G.nodes[edge[1]]['pos'][0] * 3 + G.nodes[edge[0]]['pos'][0]) / 4,
+                                    x=(G.nodes[edge[1]]['pos'][0] * 3 +
+                                       G.nodes[edge[0]]['pos'][0]) / 4,
                                     y=(G.nodes[edge[1]]['pos'][1] * 3 + G.nodes[edge[0]]['pos'][1]) / 4, xref='x', yref='y',
                                     showarrow=True,
-                                    arrowhead=3,
-                                    arrowsize=4,
+                                    arrowhead=5,
+                                    arrowsize=3,
                                     arrowwidth=1,
-                                    opacity=1
+                                    opacity=0.7
                                 ) for edge in G.edges]
                             )}
     return figure
+
+
 ######################################################################################################################################################################
 # styles: for right side hover/click component
 styles = {
@@ -174,15 +205,15 @@ styles = {
 }
 
 app.layout = html.Div([
-    #########################Title
+    # Title
     html.Div([html.H1("Transaction Network Graph")],
              className="row",
              style={'textAlign': "center"}),
-    #############################################################################################define the row
+    # define the row
     html.Div(
         className="row",
         children=[
-            ##############################################left side two input components
+            # left side two input components
             html.Div(
                 className="two columns",
                 children=[
@@ -226,7 +257,8 @@ app.layout = html.Div([
 
                             Input the account to visualize.
                             """)),
-                            dcc.Input(id="input1", type="text", placeholder="Account"),
+                            dcc.Input(id="input1", type="text",
+                                      placeholder="Account"),
                             html.Div(id="output")
                         ],
                         style={'height': '300px'}
@@ -234,14 +266,14 @@ app.layout = html.Div([
                 ]
             ),
 
-            ############################################middle graph component
+            # middle graph component
             html.Div(
                 className="eight columns",
                 children=[dcc.Graph(id="my-graph",
                                     figure=network_graph(YEAR, ACCOUNT))],
             ),
 
-            #########################################right side two output component
+            # right side two output component
             html.Div(
                 className="two columns",
                 children=[
@@ -274,16 +306,20 @@ app.layout = html.Div([
     )
 ])
 
-###################################callback for left side components
+# callback for left side components
+
+
 @app.callback(
     dash.dependencies.Output('my-graph', 'figure'),
     [dash.dependencies.Input('my-range-slider', 'value'), dash.dependencies.Input('input1', 'value')])
-def update_output(value,input1):
+def update_output(value, input1):
     YEAR = value
     ACCOUNT = input1
     return network_graph(value, input1)
     # to update the global variable of YEAR and ACCOUNT
-################################callback for right side components
+# callback for right side components
+
+
 @app.callback(
     dash.dependencies.Output('hover-data', 'children'),
     [dash.dependencies.Input('my-graph', 'hoverData')])
@@ -296,7 +332,6 @@ def display_hover_data(hoverData):
     [dash.dependencies.Input('my-graph', 'clickData')])
 def display_click_data(clickData):
     return json.dumps(clickData, indent=2)
-
 
 
 if __name__ == '__main__':
